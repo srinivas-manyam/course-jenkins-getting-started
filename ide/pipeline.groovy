0262477612ad5c85f0a7c04e582436b568c756ad
srinivas-manyam <srinivasmanyam000@gmail.com>
2022-07-13 12:10:28 +0530

    initail emailthat is sent on every build

--- ide/pipeline.groovy
+++ ide/pipeline.groovy
@@ -12,15 +12,24 @@ pipeline {
             stage('Build') {
             steps {
 
-               sh './mvnw clean package'
+            //   sh './mvnw clean package'
+            sh 'false' // true
             }
 
             post {
            //     If Maven was able to run the tests, even if some of the test
            //     failed, record the test results and archive the jar file.
                 always {
-                    junit '**/target/surefire-reports/TEST-*.xml'
-                    archiveArtifacts 'target/*.jar'
+                //    junit '**/target/surefire-reports/TEST-*.xml'
+                  //  archiveArtifacts 'target/*.jar'
+                //}
+                //changed {
+                    emailext subject: "Job \'${JOB_NAME}\' (${BUILD_NUMBER}) ${currentBuild.result}",
+                    body: "Please go to ${BUILD_URL} and verify the build", 
+                    compressLog: true, 
+                    attachLog: true, 
+                    to: "test@jenkins",
+                    recipientProviders: [upstreamDevelopers(), requestor()]
                 }
             }
         }
